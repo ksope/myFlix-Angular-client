8df60139a470d5bd4e8ff44db2aa2416829c36c1
ksope <105855785+ksope@users.noreply.github.com>
2023-10-04 11:26:46 +0100

    Update app.component.ts
    Create an openMoviesDialog() in the AppComponent class which will open a dialog to display all movies.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,13 @@ export class AppComponent {
     this.dialog.open(UserRegistrationFormComponent, {
       // Assigning the dialog a width
       width: '280px',
+    });
+  }
+
+  //open the dialog to display all returned movies
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px',
     });
   }
 }
