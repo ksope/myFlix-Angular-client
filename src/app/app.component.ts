da1cf185c67423c889ac6798a0b78a736f400a15
ksope <105855785+ksope@users.noreply.github.com>
2023-10-04 14:55:13 +0100

    Update AppComponent
    Update AppComponent to have all the dialog functions and Views being handled by the WelcomePage component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
 import { MovieCardComponent } from './movie-card/movie-card.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @Component({
   selector: 'app-root',
@@ -12,28 +10,4 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  constructor(public dialog: MatDialog) {}
-  // This is the function that will open the dialog when the signup button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      // Assigning the dialog a width
-      width: '280px',
-    });
-  }
-
-  //open the dialog to display all returned movies
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '500px',
-    });
-  }
-
-     //This is the function that will open when the login button is clicked
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: '280px',
-    });
-  }
-  
 }
