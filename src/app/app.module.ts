c2bdd92d9065c380fc48f3f0b880bb9c4f65aa9d
ksope <105855785+ksope@users.noreply.github.com>
2023-10-11 13:09:21 +0100

    Added modules to app.modult.ts
    Add { MatGridListModule }, { MatListModule }, { MatTooltipModule }, { RouterModule, Routes }, { MatIconModule }, { MovieGenreComponent }, { MovieDirectorComponent }, { MovieDetailsComponent }, { ProfileComponent } modules to app.module.ts file
    
    Add and define 'welcome', 'movies', 'genre', 'director', description' and 'profile' routes to application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatListModule } from '@angular/material/list';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
@@ -18,17 +19,39 @@ import { UserRegistrationFormComponent } from './user-registration-form/user-reg
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MovieGenreComponent } from './movie-genre/movie-genre.component';
+import { MovieDirectorComponent } from './movie-director/movie-director.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: 'genre', component: MovieGenreComponent },
+  { path: 'director', component: MovieDirectorComponent },
+  { path: 'description', component: MovieDetailsComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
-  declarations: [AppComponent, UserRegistrationFormComponent, MovieCardComponent, WelcomePageComponent, UserLoginFormComponent],
+  declarations: [AppComponent, UserRegistrationFormComponent, MovieCardComponent, WelcomePageComponent, UserLoginFormComponent, MovieGenreComponent, MovieDirectorComponent, MovieDetailsComponent, ProfileComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    MatGridListModule,
     BrowserAnimationsModule,
     MatInputModule,
+    MatTooltipModule,
     MatButtonModule,
+    MatListModule,
+    MatIconModule,
     MatCardModule,
+    RouterModule.forRoot(appRoutes),
     MatFormFieldModule,
     MatDialogModule,
     MatSnackBarModule,
